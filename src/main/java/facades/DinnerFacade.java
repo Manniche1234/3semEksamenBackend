b175368bda1eb841477fb75e0567b08665d90d00
August Manniche <august.manniche@gmail.com>
2022-01-13 10:34:53 +0100

    create dinnerFacade and endpoint

--- src/main/java/facades/DinnerFacade.java
+++ src/main/java/facades/DinnerFacade.java
@@ -1,4 +1,45 @@
 package facades;
 
+import dtos.DinnerEventDTO;
+import entities.DinnerEvent;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DinnerFacade {
+
+    private static EntityManagerFactory emf;
+    private static DinnerFacade instance;
+
+    private DinnerFacade(){
+    }
+
+    public static DinnerFacade getDinnerFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new DinnerFacade();
+        }
+        return instance;
+    }
+
+
+
+    public List<DinnerEventDTO> getAllEvents (){
+        EntityManager em = emf.createEntityManager();
+        try{
+            TypedQuery<DinnerEvent> query = em.createQuery("SELECT d FROM DinnerEvent d", DinnerEvent.class);
+            List<DinnerEvent> dinnerEvents = query.getResultList();
+            List<DinnerEventDTO> dinnerEventDTOS = DinnerEventDTO.getDtos(dinnerEvents);
+            return dinnerEventDTOS;
+        }finally {
+            em.close();
+        }
+    }
+
+
+
+
 }
---
+++ src/main/java/rest/DinnerEventResource.java
@@ -0,0 +1,37 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.DinnerEventDTO;
+import facades.DinnerFacade;
+import facades.UserFacade;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
+
+@Path("/events")
+public class DinnerEventResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    @Context
+    private UriInfo context;
+    private DinnerFacade facade = DinnerFacade.getDinnerFacade(EMF);
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+    @Context
+    SecurityContext securityContext;
+
+
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllEvents() {
+        List<DinnerEventDTO> dinnerEventDTOList = facade.getAllEvents();
+
+        return gson.toJson(dinnerEventDTOList);
+    }
+}
